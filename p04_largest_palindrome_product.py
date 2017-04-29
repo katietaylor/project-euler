535f0c01a3e56835826a281a3bf7bfaf4c91bbdd
Katie <knt.taylor@gmail.com>
2017-04-29 17:58:32 +0000

    optimizing solution

--- p04_largest_palindrome_product.py
+++ p04_largest_palindrome_product.py
@@ -6,21 +6,27 @@ Find the largest palindrome made from the product of two 3-digit numbers."""
 
 def find_largest_palindrome(high=999, low=100):
 
-    palindromes = []
+    largest_palindrome = 0
     n = high
 
     while n >= low:
         m = n
+        if n * m < largest_palindrome:
+            break
 
         while m >= low:
             product = n * m
+            if product < largest_palindrome:
+                break
 
             if str(product) == str(product)[::-1]:
-                palindromes.append(product)
+                largest_palindrome = max([largest_palindrome, product])
                 break
+
             m -= 1
+
         n -= 1
 
-    return max(palindromes)
+    return largest_palindrome
 
 print find_largest_palindrome()
