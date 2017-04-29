67101df06d56c03f09e8da49467051eb9649f967
Katie <knt.taylor@gmail.com>
2017-04-29 18:31:10 +0000

    solution for summing the squared difference

--- p06_sum_square_diff.py
+++ p06_sum_square_diff.py
@@ -5,7 +5,17 @@ The square of the sum of the first ten natural numbers is,
 
 (1 + 2 + ... + 10)2 = 552 = 3025
 Hence the difference between the sum of the squares of the first ten natural
-numbers and the square of the sum is 3025 − 385 = 2640.
+numbers and the square of the sum is 3025 - 385 = 2640.
 
 Find the difference between the sum of the squares of the first one hundred
 natural numbers and the square of the sum."""
+
+nums = range(1, 101)
+
+nums_squared = [n**2 for n in nums]
+sum_of_squares = sum(nums_squared)
+
+square_of_sums = sum(nums) ** 2
+
+diff = square_of_sums - sum_of_squares
+print diff
