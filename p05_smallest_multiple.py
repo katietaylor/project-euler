2bf8a9e8a9f7c8b329dfff76b540f5f50e1a574f
Katie <knt.taylor@gmail.com>
2017-04-29 08:17:27 +0000

    implemented find_smallest_multiple in part5

--- p05_smallest_multiple.py
+++ p05_smallest_multiple.py
@@ -3,3 +3,17 @@
 
 What is the smallest positive number that is evenly divisible by all of the
 numbers from 1 to 20?"""
+
+# solution works, but not well optimized
+def find_smallest_multiple(max):
+    factors = range(1, max + 1)
+    factors = factors[::-1]
+    num = factors[-1] * factors[-2]
+
+    while True:
+        for factor in factors:
+            if num % factor != 0:
+                num += 1
+                break
+            elif factor == factors[-1]:
+                return num
