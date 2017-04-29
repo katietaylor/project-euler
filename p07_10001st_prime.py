1633679eb5c9c6ac6e66e7e7d5599f2dd6c572a6
Katie <knt.taylor@gmail.com>
2017-04-29 19:03:12 +0000

    implemented prime finder

--- p07_10001st_prime.py
+++ p07_10001st_prime.py
@@ -2,3 +2,22 @@
 that the 6th prime is 13.
 
 What is the 10 001st prime number?"""
+
+
+def is_prime(num):
+    n = int(num ** 0.5) + 1
+    while n > 1:
+        if num % n == 0:
+            return False
+        n -= 1
+    return True
+
+
+n = 3
+primes = [2]
+
+while len(primes) < 10001:
+    if is_prime(n):
+        primes.append(n)
+        print "prime # {:,}: {:}".format(len(primes), n)
+    n += 2
