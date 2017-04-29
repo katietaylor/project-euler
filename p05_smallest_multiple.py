39a32c0c9f2a78fa633355b5a7c7e949f53f33b0
Katie <knt.taylor@gmail.com>
2017-04-29 18:30:47 +0000

    optimized solution

--- p05_smallest_multiple.py
+++ p05_smallest_multiple.py
@@ -4,16 +4,22 @@
 What is the smallest positive number that is evenly divisible by all of the
 numbers from 1 to 20?"""
 
+
 # solution works, but not well optimized
 def find_smallest_multiple(max):
+
     factors = range(1, max + 1)
+    multiplier = factors[-2]
+    num = factors[-1] * multiplier
     factors = factors[::-1]
-    num = factors[-1] * factors[-2]
 
     while True:
         for factor in factors:
             if num % factor != 0:
-                num += 1
+                multiplier += 1
+                num = factors[0] * multiplier
                 break
             elif factor == factors[-1]:
                 return num
+
+print find_smallest_multiple(20)
