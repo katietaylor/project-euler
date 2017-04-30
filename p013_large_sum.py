f32e52a4b8c8610908d539d936e33c9f0d77cfe6
Katie <knt.taylor@gmail.com>
2017-04-30 21:19:05 +0000

    implemented functions to turn the 50 numbers into a list of nums and then sum each column of numbers plus the remainder from the previous column and build up a list of the numbers in the answer

---
+++ p013_large_sum.py
@@ -0,0 +1,44 @@
+"""Work out the first ten digits of the sum of one-hundred 50-digit
+numbers (saved on p013.txt)."""
+
+
+def create_list_of_nums():
+
+    all_nums = open("p013.txt")
+
+    nums = []
+
+    for line in all_nums:
+        data = line.rstrip()
+        nums.append(data)
+    all_nums.close()
+    return nums
+
+NUMBERS = create_list_of_nums()
+
+
+def sum_num_column(idx):
+
+    total = 0
+    for num in NUMBERS:
+        total += int(num[idx])
+
+    return total
+
+
+def get_column_totals():
+
+    values = []
+    remainder = 0
+    for i in range(49, -1, -1):
+        column = sum_num_column(i)
+        total_value = str(column + remainder)
+        values.append(int(total_value[-1]))
+        remainder = int(total_value[:-1])
+    print remainder
+
+    for value in list(str(remainder))[::-1]:
+        values.append(int(value))
+
+    # only need to return first 10 digits
+    return values[-1:-11:-1]
